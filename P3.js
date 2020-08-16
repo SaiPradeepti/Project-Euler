620646e17b1cdacaeedb5b9995327aa0d308f315
SaiPradeepti <69248387+SaiPradeepti@users.noreply.github.com>
2020-08-16 13:05:01 +0530

    Problem 3: Largest prime factor

---
+++ P3.js
@@ -0,0 +1,35 @@
+function largestPrimeFactor(number) {
+  let result = 0,num = number;
+  for(let i=2;i<=number;i++){
+    let count=0;
+    for(let j=1;j<=i;j++){
+      if(i%j==0){
+        count += 1;
+      }
+    }
+    if(count === 2){
+        if(number%i==0){
+      number /= i;
+      result = i;
+    }
+      }
+  }
+  return result;
+}
+console.log(largestPrimeFactor(2));
+console.log(largestPrimeFactor(3));
+console.log(largestPrimeFactor(5));
+console.log(largestPrimeFactor(7));
+console.log(largestPrimeFactor(8));
+console.log(largestPrimeFactor(13195));
+console.log(largestPrimeFactor(600851475143));
+
+// output
+// 2
+// 3
+// 5
+// 7
+// 2
+// 29
+// Potential infinite loop detected on line 3. Tests may fail if this is not changed.
+// 71
